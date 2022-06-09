7316375d72db173154f4c62dfce967a38cc477a6
bernabas <bernabas.abdeta@yahoo.com>
2022-06-09 09:15:59 -0400

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -29,7 +29,10 @@ var fight = function(enemyName) {
     }
 
     // remove enemy's health by subtracting the amount set in the playerAttack variable
-    enemyHealth = enemyHealth - playerAttack;
+    // generate random damage value based on player's attack power
+    var damage = randomNumber(playerAttack - 3, playerAttack);
+
+    enemyHealth = Math.max(0, enemyHealth - damage);
     console.log(
       playerName + ' attacked ' + enemyName + '. ' + enemyName + ' now has ' + enemyHealth + ' health remaining.'
     );
@@ -38,7 +41,7 @@ var fight = function(enemyName) {
      window.alert(enemyName + " has died!");
 
      // award player money for winning
-     playerMoney = playerMoney + 20;
+     playerMoney = Math.max(0, playerMoney - 10);
 
      // leave while() loop since enemy is dead
      break;
@@ -48,7 +51,9 @@ var fight = function(enemyName) {
   
    }
    // remove player's health by subtracting the amount set in the enemyAttack variable
-   playerHealth = playerHealth - enemyAttack;
+   var damage = randomNumber(enemyAttack -3, enemyAttack);
+
+   playerHealth = Math.max(0, playerHealth - damage);
    //Log a resulting message to the console so we know that it worked.
    console.log (
    enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
@@ -80,8 +85,7 @@ for(var i = 0; i < enemyNames.length; i++) {
    // pick new enemy to fight based on the index of the enemyNames array
    var pickedEnemyName = enemyNames[i];
    // reset enemyHealth before starting new fight
-   enemyHealth = 50;
-    // use debugger to pause script from running and check what's going on at that moment in the code
+   enemyHealth = randomNumber(40, 60);  //bugger to pause script from running and check what's going on at that moment in the code
     // debugger;
 
     // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
@@ -175,5 +179,13 @@ var shop = function () {
        break;
   }
 };
+
+// function to generate a random numeric value
+var randomNumber = function(40, 60) {
+  var value = Math.floor(Math.random() * (60 - 40 + 1) + 40);
+
+  return value;
+};
 // start the game when the page loads
+
 startGame();
